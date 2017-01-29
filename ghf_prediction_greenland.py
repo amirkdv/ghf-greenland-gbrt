3c22e94f2c3ba6d9f781ae5674bd178a44d2f9b8
Amir Kadivar <amir@amirkdv.ca>
2017-01-28 20:45:10 -0500

    breakdown main script in two
    - no more global_learning_gb_circles_gaussian
    - generic calculations and plotting utilities in ghf_prediction.py
    - all greenland related things in ghf_prediction_greenland.py

--- ghf_prediction_greenland.py
+++ ghf_prediction_greenland.py
@@ -1,37 +1,18 @@
-import sys
-import scipy
-import random
 import os.path
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-from random import randint
-from time import time
 from mpl_toolkits.basemap import Basemap
-from sklearn.metrics import mean_squared_error
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import GradientBoostingRegressor
-from math import radians, sin, cos, asin, sqrt, floor
 from circles import equi
+from ghf_prediction import (
+    plt, pd, np,
+    load_global_gris_data, save_cur_fig, save_np_object,
+    haversine_distance,
+    split, train_regressor,
+    plot_GHF_on_map,
+    plot_GHF_on_map_pcolormesh,
+    plot_GHF_on_map_pcolormesh_interp,
+    plot_test_pred_linregress,
+    plot_GHF_histogram,
+)
 
-pd.set_option('display.max_columns', 80)
-plt.ticklabel_format(useOffset=False)
-
-MAX_GHF  = 150   # max limit of ghf considered
-N_ESTIMATORS = 5000 # number of estimators for gradient boosting regressor
-
-OUT_DIR = 'global_learning_plots_gb_circles_gaussian/'
-OUT_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), OUT_DIR)
-
-GLOBAL_CSV = '1deg_all_resampled_w_missing_from_goutorbe.csv'
-GRIS_CSV = '1deg_greenland_GHF_added2.csv' # FIXME remove old csv file from repo?
-IGNORED_COLS = [
-    'OBJECTID_1', 'continent', 'lthlgy_all', 'num_in_cel', 'num_in_con',
-     'WGM2012_Ai', 'depthmoho', 'moho_Pasya', 'lithk_cona',
-    #'G_heat_pro', 'd_2hotspots', 'd_2volcano', 'crusthk_cr', 'litho_asth',
-    #'d_2trench', 'G_d_2yng_r', 'd_2ridge', 'd2_transfo'
-    ]
 MAX_ICE_CORE_DIST = 150.
 
 # The only existing data points for Greenland are at the following ice
@@ -45,67 +26,6 @@ GREENLAND = pd.DataFrame({
     'core': ['GRIP', 'GISP2', 'DYE3', 'NGRIP', 'CC',    'SASS1', 'SASS2', 'LANGSETH', 'GAP'],
 })
 GREENLAND.set_index('core')
-
-GDR_PARAMS = {
-    'loss': 'ls',
-    'learning_rate': 0.05,
-    'n_estimators': N_ESTIMATORS,
-    'subsample': 1.0,
-    'criterion': 'friedman_mse',
-    'min_samples_split': 2,
-    'min_samples_leaf': 9,
-    'min_weight_fraction_leaf': 0.0,
-    'max_depth': 4,
-    'min_impurity_split': 1e-07,
-    'init': None,
-    'random_state': 0,
-    'max_features': 0.3,
-    'alpha': 0.9,
-    #'verbose': 0,
-    'verbose': 10,
-    'max_leaf_nodes': None,
-    'warm_start': False,
-    'presort': 'auto',
-}
-
-# Reads csv source and applies general filters.
-def read_csv(path):
-    data = pd.read_csv(path, index_col=0, na_values=-999999)
-
-    data[data['lthlgy_mod'] == 0] = np.nan
-    data[data['lthlgy_mod'] == -9999] = np.nan
-    data.dropna(inplace=True)
-
-    # drop rows with out of range GHF
-    data = data.drop(IGNORED_COLS, axis=1)
-
-    return data
-
-# loads GLOBAL_CSV and GRIS_CSV, performs GRIS specific filters, and returns a
-# single DataFrame.
-def load_global_gris_data():
-    data_global = read_csv(GLOBAL_CSV)
-    data_gris = read_csv(GRIS_CSV)
-    data_gris = process_greenland_data(data_gris)
-
-    data = pd.concat([data_global, data_gris])
-    data = pd.get_dummies(data,
-                          columns=['G_u_m_vel_', 'lthlgy_mod', 'G_ther_age'])
-
-    return data
-
-# GRIS specific filters
-def process_greenland_data(data):
-    # mapping from old to new values of lthlgy_mod
-    # Legend: volcanic=1, metamorphic=2, sedimentary=3
-    lthlgy_mod_rewrites = {
-        1: 2, 2: 3, 3: 3, 4: 3, 5: 1, 6: 2, 7: 1, 8: 3, 9: 2, 10: 2
-    }
-    data['lthlgy_mod'] = data.apply(
-        lambda row: lthlgy_mod_rewrites[row['lthlgy_mod']],
-        axis=1
-    )
-    return data
 
 # Approximates GHF values at rows with unknown GHF according to a Gaussian
 # decay formula based on known GHF values in GREENLAND.
@@ -145,229 +65,9 @@ def fill_in_greenland_GHF(data):
     data.dropna(inplace=True)
     return data, gris_unknown
 
-# returns a pair of DataFrames: one containing rows in data that are closer
-# than radius to center, and those that are not.
-def split_by_distance(data, center, radius):
-    # store distances in a temporary column '_distance'
-    data['_distance'] = haversine_distance(data, center)
-    within = data[data._distance < radius].drop('_distance', axis=1)
-    beyond = data[data._distance > radius].drop('_distance', axis=1)
-    data.drop('_distance', axis=1, inplace=True)
-
-    return within, beyond
-
-# returns a column containing distances of each row of data from center
-def haversine_distance(data, center):
-    def _haversine(row):
-        lon1, lat1 = center
-        lon2, lat2 = row['Longitude_1'], row['Latitude_1']
-        lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
-        dlon = lon2 - lon1
-        dlat = lat2 - lat1
-        a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
-        c = 2 * asin(sqrt(a))
-        km = 6367 * c
-        return km
-
-    return data.apply(_haversine, axis=1)
-
-# splits rows in data into a training and test set according to the following
-# rule: consider a circle C centered at center and with radius max_dist. The
-# training set is those rows outside C and a randomly chosen subset of those
-# rows within C (proportion of points in C kept for test is given by
-# test_size; float between 0 and 1).
-def split(data, center, test_size=.15, max_dist=3500):
-    data_test, data_train = split_by_distance(data, center, max_dist)
-    additional_train, data_test = train_test_split(
-        data_test, random_state=0, test_size=test_size
-    )
-    data_train = pd.concat([data_train, additional_train])
-
-    X_train, y_train = data_train.drop('GHF', axis=1), data_train['GHF']
-    X_test,  y_test  = data_test.drop('GHF', axis=1),  data_test['GHF']
-
-    return X_train, y_train, X_test, y_test
-
-# plots a series of GHF values at given latitude and longitude positions
-def plot_GHF_on_map(m, lons, lats, values,
-                    parallel_step=20., meridian_step=60.,
-                    clim=(20., 150.), clim_step=10,
-                    colorbar_args={}, scatter_args={}):
-    m.drawparallels(
-        np.arange(-80., 81., parallel_step), labels=[1, 0, 0, 0], fontsize=10
-    )
-    m.drawmeridians(
-        np.arange(-180., 181., meridian_step), labels=[0, 0, 0, 1], fontsize=10
-    )
-    m.drawmapboundary(fill_color='white')
-    m.drawcoastlines(linewidth=0.5)
-
-    x, y = m(lons, lats)
-
-    cs = m.scatter(x, y, c=values, **scatter_args)
-
-    cbar = m.colorbar(cs, **colorbar_args)
-    cbar.set_label('mW m$^{-2}$')
-    labels = range(int(clim[0]), int(clim[1]) + 1, clim_step)
-    cbar.set_ticks(labels)
-    cbar.set_ticklabels(labels)
-    plt.clim(*clim)
-
-# plots a series of GHF values at given latitude and longitude positions in
-# ascii format
-def plot_GHF_on_map_pcolormesh(m, lons, lats, values,
-                    parallel_step=20., meridian_step=60.,
-                    clim=(20., 150.), clim_step=10,
-                    colorbar_args={}, pcolor_args={}):
-    m.drawparallels(
-        np.arange(-80., 81., parallel_step), labels=[1, 0, 0, 0], fontsize=10
-    )
-    m.drawmeridians(
-        np.arange(-180., 181., meridian_step), labels=[0, 0, 0, 1], fontsize=10
-    )
-    m.drawmapboundary(fill_color='white')
-    m.drawcoastlines(linewidth=0.5)
-
-    lons_min, lons_max = np.min(lons), np.max(lons)
-    lats_min, lats_max = np.min(lats), np.max(lats)
-
-    lons_array = np.linspace(lons_min,lons_max,lons_max-lons_min+1)
-    lats_array = np.linspace(lats_min,lats_max,lats_max-lats_min+1)
-
-    lon, lat = np.meshgrid(lons_array, lats_array)
-
-    x, y = m(lon, lat)
-
-    ascii = np.zeros([len(lats_array),len(lons_array)])
-    ascii = np.where(ascii==0,np.nan,0)
-
-    for item in np.vstack([lons,lats,values]).T:
-        j = np.floor(lons_array).tolist().index(floor(item[0]))
-        i = np.floor(lats_array).tolist().index(floor(item[1]))
-        ascii[i][j] = item[2]
-
-    ascii = np.ma.masked_where(np.isnan(ascii),ascii)
-
-    cs = m.pcolormesh(x,y,ascii,**pcolor_args)
-
-    cbar = m.colorbar(cs, **colorbar_args)
-    cbar.set_label('mW m$^{-2}$')
-    labels = range(int(clim[0]), int(clim[1]) + 1, clim_step)
-    cbar.set_ticks(labels)
-    cbar.set_ticklabels(labels)
-    plt.clim(*clim)
-
-# plots a series of GHF values at given latitude and longitude positions in
-# ascii format interpolated using basemap transform_scalar functions
-def plot_GHF_on_map_pcolormesh_interp(m, lons, lats, values,
-                    parallel_step=20., meridian_step=60.,
-                    clim=(20., 150.), clim_step=10,
-                    colorbar_args={}, pcolor_args={}):
-    m.drawparallels(
-        np.arange(-80., 81., parallel_step), labels=[1, 0, 0, 0], fontsize=10
-    )
-    m.drawmeridians(
-        np.arange(-180., 181., meridian_step), labels=[0, 0, 0, 1], fontsize=10
-    )
-    m.drawmapboundary(fill_color='white')
-    m.drawcoastlines(linewidth=0.5)
-
-    lons_min, lons_max = np.min(lons), np.max(lons)
-    lats_min, lats_max = np.min(lats), np.max(lats)
-
-    lons_array = np.linspace(lons_min,lons_max,lons_max-lons_min+1)
-    lats_array = np.linspace(lats_min,lats_max,lats_max-lats_min+1)
-
-    lon, lat = np.meshgrid(lons_array, lats_array)
-
-    x, y = m(lon, lat)
-
-    ascii = np.zeros([len(lats_array),len(lons_array)])
-    ascii = np.where(ascii==0,np.nan,0)
-
-    for item in np.vstack([lons,lats,values]).T:
-        j = np.floor(lons_array).tolist().index(floor(item[0]))
-        i = np.floor(lats_array).tolist().index(floor(item[1]))
-        ascii[i][j] = item[2]
-
-    ascii_interp, x, y = m.transform_scalar(
-        ascii, lons_array, lats_array,
-        5*len(lons_array), 5*len(lats_array), returnxy=True
-    )
-
-    ascii_interp = np.ma.masked_where(np.isnan(ascii_interp),ascii_interp)
-
-    cs = m.pcolormesh(x,y,ascii_interp,**pcolor_args)
-
-    cbar = m.colorbar(cs, **colorbar_args)
-    cbar.set_label('mW m$^{-2}$')
-    labels = range(int(clim[0]), int(clim[1]) + 1, clim_step)
-    cbar.set_ticks(labels)
-    cbar.set_ticklabels(labels)
-    plt.clim(*clim)
-
-# saves current matplotlib plot to given filename in OUT_DIR
-def save_cur_fig(filename, title=None):
-    plt.title(title)
-    plt.savefig(os.path.join(OUT_DIR, filename), dpi=400)
-    sys.stderr.write('Saved %s to %s.\n' % (repr(title), filename))
-    plt.clf()
-
-# Trains and returns a GradientBoostingRegressor over the given training
-# feature and value vectors. Feature importance values are stored in
-# OUTDIR/logfile
-def train_regressor(X_train, y_train, logfile=None):
-    sys.stderr.write('-> Training ...')
-    start = time()
-    reg = GradientBoostingRegressor(**GDR_PARAMS)
-    reg.fit(X_train, y_train)
-    sys.stderr.write(' (%.2f secs)\n' % (time() - start))
-
-    importance = reg.feature_importances_
-    hdrs = list(X_train.columns.values)
-    logs = np.asarray(sorted(zip(hdrs, importance), key=lambda x: x[1]))
-    if logfile:
-        np.savetxt(os.path.join(OUT_DIR, logfile), logs, fmt="%s")
-        sys.stderr.write('-> Saved feature importances to %s.\n' % (logfile))
-
-    return reg
-
-def error_summary(y_test, y_pred):
-    _, _, r_value, _, _= scipy.stats.linregress(y_test, y_pred)
-    rmse = mean_squared_error(y_test, y_pred)
-    return r_value ** 2, rmse # FIXME divide rmse by sd(y_test)
-
-# plots the linear regression of two GHF value series (known test values and
-# predicted values) and saves the plot to OUT_DIR/filename.
-def plot_test_pred_linregress(y_test, y_pred, filename, title=None):
-    r2, rmse = error_summary(y_test, y_pred)
-
-    plt.scatter(y_test, y_pred, label='tests, r$^2$=%f' % r2)
-    plt.grid(True)
-    plt.axes().set_aspect('equal')
-    plt.xlabel('$GHF$')
-    plt.ylabel('$\widehat{GHF}$')
-    plt.xlim([0, MAX_GHF])
-    plt.ylim([0, MAX_GHF])
-
-    title = title + '\n$r^2=%.3f, RMSE=%.2f$' % (r_value**2, rmse**0.5)
-    # FIXME pull setting title out of function; requires save_cur_fig to not
-    # set title instead let the plot functions do it.
-    save_cur_fig(filename, title=title)
-
-# plots the histogram of given GHF values
-def plot_GHF_histogram(values, max_density=.05):
-    # FIXME increase number of bins
-    plt.hist(values, bins=np.linspace(0, 150, 16), lw=2, c='blue', normed=True)
-    plt.xlabel('GHF (mW m$^{-2}$)')
-    plt.ylabel('Normalized Frequency')
-    plt.grid(True)
-    plt.xlim([0, MAX_GHF])
-    plt.ylim([0, max_density])
-
 # saves GRIS known and predicted GHF and latitude/longitude and values as a
 # numpy array in OUT_DIR/filename.
-def save_gris_prediction_data(gris_unknown, gris_known, filename):
+def save_gris_prediction_data(gris_unknown, gris_known, y_gris, filename):
     lats = np.hstack([gris_unknown.Longitude_1.as_matrix(),
                       gris_known.Longitude_1.as_matrix()])
     lons = np.hstack([gris_unknown.Latitude_1.as_matrix(),
@@ -379,30 +79,9 @@ def save_gris_prediction_data(gris_unknown, gris_known, filename):
     final[:, 1] = lons
     final[:, 2] = ghfs
 
-    path = os.path.join(OUT_DIR, filename)
-    np.savetxt(path, final, delimiter=', ',
-               header='lon, lat, ghf', fmt='%10.5f')
-    sys.stderr.write('Saved gris data to %s\n' % filename)
+    save_np_object(filename, 'gris data', final, delimiter=', ',
+                   header='lon, lat, ghf', fmt='%10.5f')
 
-def eval_prediction_multiple(data, tasks):
-    return {task: eval_prediction(data, *task) for task in tasks}
-
-def eval_prediction(data, test_size, max_dist, center):
-    X_train, y_train, X_test, y_test = \
-        split(data, center, test_size=test_size, max_dist=max_dist)
-    if X_test.empty:
-        return None, None
-
-    reg = train_regressor(X_train.drop(['Latitude_1', 'Longitude_1'], axis=1),
-                          y_train)
-    y_pred = reg.predict(X_test.drop(['Latitude_1', 'Longitude_1'], axis=1))
-    return error_summary(y_test, y_pred)
-
-def random_prediction_ctr():
-    return randint(-100, 50), randint(45, 90)
-
-# Evaluate Gradient Boosting Regressor
-# ====================================
 
 # Create average RMSE plots for different test_size and max_dist values
 # ---------------------------------------------------------------------
@@ -448,7 +127,7 @@ save_cur_fig('GHF_1deg_averaged_map_test.png', title='GHF at test set')
 # Predict GHF over test set
 # -----------------------------
 reg = train_regressor(X_train.drop(['Latitude_1', 'Longitude_1'], axis=1),
-                      y_train, 'GHF_1deg_averaged_logfile.txt')
+                      y_train, logfile='GHF_1deg_averaged_logfile.txt')
 y_pred = reg.predict(X_test.drop(['Latitude_1', 'Longitude_1'], axis=1))
 
 m = Basemap(width=1600000, height=2650000, resolution='l',
@@ -626,4 +305,4 @@ save_cur_fig('hist_global.png', title='GHF global measurement')
 
 # Store greenland predictions and known values for ARC GIS
 # --------------------------------------------------------
-save_gris_prediction_data(X_gris, gris_known, 'lat_lon_ghf.txt')
+save_gris_prediction_data(X_gris, gris_known, y_gris, 'lat_lon_ghf.txt')
